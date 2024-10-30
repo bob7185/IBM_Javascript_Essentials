4aecf6aa885593b6fb8a7fce61318b57ea488c6d
bob7185 <stevejean285@gmail.com>
2024-10-30 14:54:59 -0400

    Final version

--- healthArticle/health_article.js
+++ healthArticle/health_article.js
@@ -3,12 +3,12 @@ const url = './health_article.json';
 
 xhr.open('GET', url, true);
 xhr.responseType = 'json';
-xhr.onload = function (){
+xhr.onload = function () {
     let articles = xhr.response.articles;
     let articlesDiv = document.getElementById("articles");
 
-    articles.array.forEach(article => {
-       const articleDiv = document.createElement('div');
+    articles.forEach((article) => {
+        const articleDiv = document.createElement('div');
         articleDiv.classList.add('article');
         const title = document.createElement('h2');
         title.textContent = article.title;
@@ -16,21 +16,21 @@ xhr.onload = function (){
         const description = document.createElement('p');
         description.textContent = article.description;
 
-        const waysheader = docuemtn.createElement('h3');
+        const waysheader = document.createElement('h3');
         waysheader.textContent = 'Ways to Achieve:';
 
         const waysList = document.createElement('ul');
         article.ways_to_achieve.forEach(way => {
             const listItem = document.createElement('li');
-            listitem.textContent = way;
+            listItem.textContent = way;
             waysList.appendChild(listItem);
         });
 
         const benefitsHeader = document.createElement('h3');
-        benefitsHeader. textContent = 'Benefits';
+        benefitsHeader.textContent = 'Benefits';
 
         const benefitsList = document.createElement('ul');
-        article.benefits.forEach( benefit =>{
+        article.benefits.forEach(benefit => {
             const listItem = document.createElement('li');
             listItem.textContent = benefit;
             benefitsList.appendChild(listItem);
@@ -43,7 +43,7 @@ xhr.onload = function (){
 
         articlesDiv.appendChild(articleDiv);
 
-        
+
     });
 }
 xhr.send();
